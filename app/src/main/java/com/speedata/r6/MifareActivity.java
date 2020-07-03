eca7a84f054c9974a4b0a457786cdb84943d99ba
754995643 <754995643@qq.com>
2020-07-03 17:24:40 +0800

    修改mifare测试流程，上传到mdm，更新版本到1.1

--- app/src/main/java/com/speedata/r6/MifareActivity.java
+++ app/src/main/java/com/speedata/r6/MifareActivity.java
@@ -122,6 +122,59 @@ public class MifareActivity extends AppCompatActivity implements OnClickListener
 			main_info.append(getString(R.string.msg_mifare_ok_auth));
 			main_info.append("\n\n");
 			
+
+			//wrtie a value to block
+			if (dev.WriteBlockValue(block, 66) != 0) {
+				main_info.append(getString(R.string.msg_mifare_error_writevalue));
+				return;
+			}
+			main_info.append(getString(R.string.msg_mifare_ok_writevalue));
+			main_info.append(String.format(" %d\n\n", 66));
+
+			//read the value
+			int value[] = dev.ReadBlockValue(block);
+			if (value == null) {
+				main_info.append(getString(R.string.msg_mifare_error_readvalue));
+				return;
+			}
+			main_info.append(getString(R.string.msg_mifare_ok_readvalue));
+			main_info.append(String.format(" %d\n\n", value[0]));
+
+			//increment the block's value
+			if (dev.IncrementBlockValue(block, 12) != 0) {
+				main_info.append(getString(R.string.msg_mifare_error_incvalue));
+				return;
+			}
+			main_info.append(getString(R.string.msg_mifare_ok_incvalue));
+			main_info.append(String.format(" %d\n\n", 12));
+
+			//read the value again
+			value = dev.ReadBlockValue(block);
+			if (value == null) {
+				main_info.append(getString(R.string.msg_mifare_error_readvalue));
+				return;
+			}
+			main_info.append(getString(R.string.msg_mifare_ok_readvalue));
+			main_info.append(String.format(" %d\n\n", value[0]));
+
+			//decrement the block's value
+			if (dev.DecrementBlockValue(block, 55) != 0) {
+				main_info.append(getString(R.string.msg_mifare_error_decvalue));
+				return;
+			}
+			main_info.append(getString(R.string.msg_mifare_ok_decvalue));
+			main_info.append(String.format(" %d\n\n", 55));
+
+			//read the value again
+			value = dev.ReadBlockValue(block);
+			if (value == null) {
+				main_info.append(getString(R.string.msg_mifare_error_readvalue));
+				return;
+			}
+			main_info.append(getString(R.string.msg_mifare_ok_readvalue));
+			main_info.append(String.format(" %d\n\n", value[0]));
+
+
 			//write data to block directly
 			byte[] data = new byte[16];
 			StringBuilder dataString = new StringBuilder();
@@ -136,7 +189,7 @@ public class MifareActivity extends AppCompatActivity implements OnClickListener
 			main_info.append(getString(R.string.msg_mifare_ok_writeblock));
 			main_info.append(dataString.toString());
 			main_info.append("\n\n");
-			
+
 			//read data from the same block
 			byte[] getdata = dev.ReadBlock(block);
 			if (getdata == null) {
@@ -150,58 +203,8 @@ public class MifareActivity extends AppCompatActivity implements OnClickListener
 			main_info.append(getString(R.string.msg_mifare_ok_readblock));
 			main_info.append(getdataString.toString());
 			main_info.append("\n\n");
-			
-			//wrtie a value to block
-			if (dev.WriteBlockValue(block, 66) != 0) {
-				main_info.append(getString(R.string.msg_mifare_error_writevalue));
-				return;
-			}
-			main_info.append(getString(R.string.msg_mifare_ok_writevalue));
-			main_info.append(String.format(" %d\n\n", 66));
-			
-			//read the value
-			int value[] = dev.ReadBlockValue(block);
-			if (value == null) {
-				main_info.append(getString(R.string.msg_mifare_error_readvalue));
-				return;
-			}
-			main_info.append(getString(R.string.msg_mifare_ok_readvalue));
-			main_info.append(String.format(" %d\n\n", value[0]));
-			
-			//increment the block's value
-			if (dev.IncrementBlockValue(block, 12) != 0) {
-				main_info.append(getString(R.string.msg_mifare_error_incvalue));
-				return;
-			}
-			main_info.append(getString(R.string.msg_mifare_ok_incvalue));
-			main_info.append(String.format(" %d\n\n", 12));
-			
-			//read the value again
-			value = dev.ReadBlockValue(block);
-			if (value == null) {
-				main_info.append(getString(R.string.msg_mifare_error_readvalue));
-				return;
-			}
-			main_info.append(getString(R.string.msg_mifare_ok_readvalue));
-			main_info.append(String.format(" %d\n\n", value[0]));
-			
-			//decrement the block's value
-			if (dev.DecrementBlockValue(block, 55) != 0) {
-				main_info.append(getString(R.string.msg_mifare_error_decvalue));
-				return;
-			}
-			main_info.append(getString(R.string.msg_mifare_ok_decvalue));
-			main_info.append(String.format(" %d\n\n", 55));
-			
-			//read the value again
-			value = dev.ReadBlockValue(block);
-			if (value == null) {
-				main_info.append(getString(R.string.msg_mifare_error_readvalue));
-				return;
-			}
-			main_info.append(getString(R.string.msg_mifare_ok_readvalue));
-			main_info.append(String.format(" %d\n\n", value[0]));
-			
+
+
 			//halt current card
 			if (dev.HaltCard() != 0) {
 				main_info.append(getString(R.string.msg_mifare_error_haltcard));
